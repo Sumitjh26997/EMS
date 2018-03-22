d5f0fa99b7ac6e906c92d9e64575f5b0cc3efdfb
Sumitjh26997 <sumit.hotchandnai@gmail.com>
2018-03-22 17:59:27 +0530

    AdminLTE dependencies

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,37 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/bootstrap3-wysihtml5.min.css',
+        'css/daterangepicker.css',
+        'css/bootstrap-datepicker.min.css',
+        'css/jquery-jvectormap.css',
+        'css/morris.css',
+        'css/skins/_all-skins.min.css',
+        'css/AdminLTE.min.css',
+        'css/ionicons.min.css',
+        'css/font-awesome.min.css',
+        'css/bootstrap.min.css',
         'css/site.css',
     ];
     public $js = [
+        'js/demo.js',
+        'js/dashboard.js',
+        'js/adminlte.min.js',
+        'js/fastclick.js',
+        'js/jquery.slimscroll.min.js',
+        'js/bootstrap3-wysihtml5.all.min.js',
+        'js/bootstrap-datepicker.min.js',
+        'js/daterangepicker.js',
+        'js/moment.min.js',
+        'js/jquery.knob.min.js',
+        'js/jquery-jvectormap-world-mill-en.js',
+        'js/jquery-jvectormap-1.2.2.min.js',
+        'js/jquery.sparkline.min.js',
+        'js/morris.min.js',
+        'js/raphael.min.js',
+        'js/bootstrap.min.js',
+        'js/jquery-ui.min.js',
+        'js/jquery.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
