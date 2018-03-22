e28450f38fc50dccf0871ddb78bf8898a145f9ae
yashkondawar <yashmkondawar73@gmail.com>
2018-03-22 23:24:01 +0530

    No title and notifbar

--- App/app/src/main/java/com/example/yashkondawar/ems/MainActivity.java
+++ App/app/src/main/java/com/example/yashkondawar/ems/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.yashkondawar.ems;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
--- App/app/src/main/java/com/example/yashkondawar/ems/SplashScreen.java
+++ App/app/src/main/java/com/example/yashkondawar/ems/SplashScreen.java
@@ -10,6 +10,7 @@ import android.os.Handler;import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -17,6 +18,8 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_spalsh_screen);
 
         handler=new Handler();
