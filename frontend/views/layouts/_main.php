28dfdc018017fa0d4d567e84037cb1da71fcf35a
Shikhar Bhatt <shikhar.bhatt011@gmail.com>
2018-03-22 23:08:52 +0530

    Favicon Fixed

--- frontend/views/layouts/_main.php
+++ frontend/views/layouts/_main.php
@@ -21,6 +21,7 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+
     <link rel="icon" href="mospi.webp" type="image">
     <?php $this->head() ?>
 </head>
