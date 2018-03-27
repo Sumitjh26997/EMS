94b89132ae139e3848477eeaab640e343da8d2c5
Sumitjh26997 <sumit.hotchandani@gmail.com>
2018-03-27 22:54:29 +0530

    Date Picker Integrated

--- frontend/views/program/_form.php
+++ frontend/views/program/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
+
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Program */
@@ -14,11 +16,24 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'type')->textInput(['maxlength' => true]) ?>
+
     <?= $form->field($model, 'location')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'timestamp')->textInput() ?>
+    <!-- <?= $form->field($model, 'timestamp')->textInput() ?> -->
 
-    <?= $form->field($model, 'date')->textInput() ?>
+    <?= $form->field($model, 'date')->widget(
+        DatePicker::className(), [
+            // inline too, not bad
+             'inline' => false,
+             // modify template for custom rendering
+            //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+            'clientOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd'
+            ]
+    ]);?>
+    <!-- <?= $form->field($model, 'date')->textInput() ?> -->
 
     <?= $form->field($model, 'start_time')->textInput() ?>
 
@@ -26,13 +41,12 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'init_weight')->textInput() ?>
+    <!-- <?= $form->field($model, 'init_weight')->textInput() ?> -->
 
     <?= $form->field($model, 'priority')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'type')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'program_weight')->textInput() ?>
+    <!-- <?= $form->field($model, 'program_weight')->textInput() ?> -->
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
